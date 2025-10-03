f44a63ed461a6fcd03a6d39d05792f4e22db783e
SussieFrantzen <cph-sf252@stud.ek.dk>
2025-10-03 15:29:19 +0200

    task1. kan ikke afspilles

---
+++ Task1/src/Customer.java
@@ -0,0 +1,77 @@
+/* 1.a Lav en klasse, Customer, med attributterne (felterne/instansvariablene):
+String firstName
+String lastName
+String username
+int id
+static int counter
+*/
+
+import java.util.ArrayList;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private int id;
+    private static int counter =0;
+
+    /* -------------------------------------//-----------------------------------------------*/
+    // 1.b Klassen skal have en konstruktør, der tager kundens navn og brugernavn som parametre.
+
+    public Customer(String firstName, String lastName, String userName) {
+        setFirstName (firstName);
+        setUserName (userName);
+        setLastName (lastName);
+        counter++;
+        this.id = counter;
+
+    }
+
+    /* -------------------------------------//-----------------------------------------------*/
+    // 1.c Sørg for at counter tælles op med 1, hver gang konstruktoren bliver kaldt.
+    // Brug counter til at initialisere id
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstName;
+    }
+        public void setFirstName(String firstName) {
+            if(firstName != null && !firstName.equals("") && firstName.length() > 1) {
+                this.firstName = firstName;
+            }
+        }
+
+        public String getLastname() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        /* -------------------------------------//-----------------------------------------------*/
+        // 1.d Giv klassen en toString() metode,
+        // der printer kundens detaljer pænt ud.
+        // Gør alle klassens felter private, og tilføj getters og setters.
+
+
+        public String toString() {
+            return "Navn: " + firstName + " " + lastName + ", username: " + userName;
+
+            /* -------------------------------------//-----------------------------------------------*/
+        }
+}
--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,35 +1,42 @@
-/* 1.a Lav en klasse, Customer, med attributterne (felterne/instansvariablene):
-String firstName
-String lastName
-String username
-int id
-static int counter
+import java.util.ArrayList;
 
-1.b Klassen skal have en konstruktør, der tager kundens navn og brugernavn som parametre.
-
-1.c Sørg for at counter tælles op med 1, hver gang konstruktoren bliver kaldt.
-Brug counter til at initialisere id
-
-
-1.d Giv klassen en toString() metode,
-der printer kundens detaljer pænt ud. Gør alle klassens felter private, og tilføj getters og setters.
-
-1.e Skriv en Main klasse med en main-metode, hvor der oprettes en ArrayList,
-som du kalder 'customers'.
+    public class Main{
+        /*
+        1.e Skriv en Main klasse med en main-metode, hvor der oprettes en ArrayList,
+        som du kalder 'customers'.
         (Du kan oprette instanserne først, og så add'e dem til listen.
-         Du kan også instantiere og add'e i samme linie.)
+        Du kan også instantiere og add'e i samme linie.)*/
+    public static void main(String[] args) {
 
-        1.f Skriv en static metode i Main kaldet printCustomers(ArrayList customers),
-         hvor du printer alle kunderne ud ved at gennemløbe customers med et for each loop.
-         Test metoden fra main ved at kalde den med din ArrayList som argument.
+        /* -------------------------------------//-----------------------------------------------*/
+        // 1.f Skriv en static metode i Main kaldet printCustomers(ArrayList customers),
+        // hvor du printer alle kunderne ud ved at gennemløbe customers med et for each loop.
+        // Test metoden fra main ved at kalde den med din ArrayList som argument.
+        //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+        // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+        Customer c = new Customer("Bruce", "Wayne", "Batman");
+        c.setFirstName("");
 
- */
-    public class Main {
-        public static void main(String[] args) {
+        ArrayList<Customer> customers = new ArrayList<>();
+        customers.add(c);
+        customers.add(new Customer("Tony", "Stark", "Iron man"));
+        customers.add(new Customer("Barry", "Allen", "The Flash"));
 
+        printCustomers(customers);
+        c.setFirstName("Ken");
 
-         }
+        printCustomers(customers);
+
+        int count = Customer.getCounter();
+        String name = c.getFirstname();
+
     }
+
+    public static void printCustomers(ArrayList<Customer> customers) {
+        for (Customer cs : customers) {
+            System.out.println(cs);
+            /* -------------------------------------//-----------------------------------------------*/
+        }
+    }
+}
